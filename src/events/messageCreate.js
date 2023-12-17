0a9e916e5e021fb98a20946939c690d948997f2f
eristocrates <reillocb@gmail.com>
2023-12-16 23:58:54 -0500

    Filter out channel follow add

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -2,26 +2,22 @@
 
 const { Events } = require("discord.js");
 
-// TODO: fill this in with webhook ids that correspond to their games
-// const webhookIdDictionary = {
-//     "": ""
-// };
 // const outputChannelId = "1178390170609918062"; rhythm-game-news
+// const inputChannelIds = ["1185454559385100338", "1185454663374471278", "1185454691824435294"];
 const outputChannelId = "1185803977623863356"; // test-news
 const inputChannelIds = ["1185798622772744274"];
-// const inputChannelIds = ["1185454559385100338", "1185454663374471278", "1185454691824435294"];
 
 module.exports = {
     name: Events.MessageCreate,
     once: false,
     async execute(message) {
         if (!inputChannelIds.includes(message.channel.id)) return;
+        if (message.type === 12) return;
 
         const outputChannel = await message.guild.channels.fetch(outputChannelId);
         const attachments = message.attachments.map(attachment => attachment);
         console.log(message);
 
-        // TODO: replace (game) with game name obtained from webhookIdDictionary
         outputChannel.send({ 
             // TODO: add randomized funny replacements for "news update from"
             content: `**[NEWS UPDATE FROM ${message.author.username} ]** \n\n ${message.content}`,
