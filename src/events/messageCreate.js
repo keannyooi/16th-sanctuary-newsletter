46efecf7e232b4e21af8b7ceb874ef458cc8d405
eristocrates <reillocb@gmail.com>
2023-12-20 01:26:18 -0500

    Added Ace combat references

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -45,6 +45,10 @@ function getRandomAnnouncement(source) {
         `Play ${source} to relax your fingers and improve brain coordination!`,
         `Hit me with the HARDCORE ${source} update!!!`,
         `Funding for this ${source} update was made possible by viewers like you`,
+        // Ace Combat References
+        `There's no mercy in ${source}. People live and people die. That's all there is to it.`,
+        `Yo ${source}, still alive?`,
+        `"Too bad buddy. This twisted game needs to be reset. We'll start over from 'Zero' with this ${source} update and entrust the future to the next generation`,
         // bms references
         `I'm sorry for dragging you all the way, I just hope you're interested in ${source}...`, // baa-baa-baa-baa
         `Hello-안녕-salut! Luna Fozer here bringing you an update from ${source}!`,
